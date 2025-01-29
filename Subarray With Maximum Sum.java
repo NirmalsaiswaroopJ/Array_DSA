6af6787ae5c6615ccf452c462fa8ad6df0c0eb22
NirmalsaiswaroopJ <128392324+NirmalsaiswaroopJ@users.noreply.github.com>
2025-01-29 18:49:34 +0530

    Create Subarray With Maximum Sum.java

---
+++ Subarray With Maximum Sum.java
@@ -0,0 +1,15 @@
+// dp approach time complexity is O(n) and space complexity is also O(n).
+public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        if (n == 0){
+            return 0;
+        }
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int maxNumber = nums[0];
+        for(int i = 1; i< n; i++){
+            dp[i] = Math.max(dp[i-1]+nums[i], nums[i]);
+            maxNumber = Math.max(maxNumber, dp[i]);
+        }
+        return maxNumber;
+}
